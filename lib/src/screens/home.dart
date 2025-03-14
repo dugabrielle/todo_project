d1d90086e0eb0e36f4c92a641731fa26427e3ff6
Gabi <dugabrielle9@gmail.com>
2025-03-14 20:26:08 -0300

    add appBar

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -8,8 +8,60 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _darkMode = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: appBarMethod(),
+    );
+  }
+
+  appBarMethod() {
+    return AppBar(
+      toolbarHeight: 80,
+      elevation: 0,
+      title: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                'ToDo App',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              _darkMode ? Icons.nightlight_round : Icons.wb_sunny,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              _alternarTema();
+            },
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const SizedBox.shrink(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _alternarTema() {
+    setState(() {
+      _darkMode = !_darkMode;
+    });
   }
 }
