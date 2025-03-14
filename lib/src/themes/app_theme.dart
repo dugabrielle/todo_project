826b49b2a65a4843b46b0e0a0b4fca8e9eab7164
Gabi <dugabrielle9@gmail.com>
2025-03-14 18:43:25 -0300

    update theme

--- lib/src/themes/app_theme.dart
+++ lib/src/themes/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
@@ -12,6 +13,37 @@ class AppTheme {
     colorScheme: ColorScheme.light(
       primary: Colors.deepPurple,
       secondary: Colors.purple.shade300,
+    ),
+    textTheme: GoogleFonts.comicNeueTextTheme(
+      TextTheme(
+        bodyMedium: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ),
+  );
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: const Color(0xFF1E1E1E),
+    primaryColor: Colors.purple.shade700,
+    appBarTheme: AppBarTheme(
+      backgroundColor: const Color(0xFF9673D2),
+      foregroundColor: Colors.white,
+    ),
+    colorScheme: ColorScheme.dark(
+      primary: Colors.purple.shade600,
+      secondary: Colors.purple.shade400,
+    ),
+    textTheme: GoogleFonts.comicNeueTextTheme(
+      TextTheme(
+        bodyMedium: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
     ),
   );
 }
