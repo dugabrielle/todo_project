190b858798ee3c38140fcab710c983a24c38439f
Gabi <dugabrielle9@gmail.com>
2025-03-14 17:28:24 -0300

    projeto task e criação da home

---
+++ lib/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _nameState();
+}
+
+class _nameState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
